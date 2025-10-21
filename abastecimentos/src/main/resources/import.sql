168642fcc6a62cd2d0fa5d2bd95821452dfd3979
Yan Hideki Kawakami <YanHidekiKawakami@MacBook-Pro-de-Yan.local>
2025-10-20 21:43:40 -0300

    add: adicao da persistencia de dados

--- abastecimentos/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
--- Insert na tablea de combustíveis
-INSERT INTO tb_combustiveis (id, nome_combustivel, preco_por_litro) VALUES (1, 'GASOLINA', 5.99);
-INSERT INTO tb_combustiveis (id, nome_combustivel, preco_por_litro) VALUES (2, 'ETANOL', 3.99);
